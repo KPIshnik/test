6fe602ede49fa2e614a04d66f397262392ecc6a9
KPIshnik <dimoglo.anton@gmail.com>
2022-05-13 17:46:38 +0000

    started server

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('Aloha world! ') 
-console.log('is huitta happening? ')
+const http= require('http')
+
+let reqCounter = 0;
+
+const server = http.createServer( (req,res)=> {
+    reqCounter++
+    res.write('Aloha Mother Fuker\n')
+    res.write('Huita is happening \n')
+    res.write(`${reqCounter}`)
+    res.end()
+})            
+
+server.listen(5500)
