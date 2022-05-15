29ae00eb1d0f6714d618bd90d3762320a0894d96
KPIshnik <dimoglo.anton@gmail.com>
2022-05-15 10:58:48 +0000

    made switch case for route

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http= require('http')
 
 let reqCounter = 0;
-
+let ans=''
 const server = http.createServer( (req,res)=> {
+        
     reqCounter++
-    res.write('Aloha Mother Fuker\n')
-    res.write('Huita is happening \n')
-    res.write(`${reqCounter}`)
+    switch (req.url){
+        case '/':
+            res.write('Aloha Mather Fucker! \n')
+            break
+        case '/huita':
+            res.write('Huita is hapening \n')
+            break
+        case '/mozg':
+            res.write('mozg is good :) \n')
+            break
+        case '/favicon.ico':
+            reqCounter--    
+            res.write('favicon')
+            break
+        default:
+            res.write('404 not found \n') 
+            break
+    }
+    
+    res.write(`Zaebal ${reqCounter} raz`)
     res.end()
 })            
 
