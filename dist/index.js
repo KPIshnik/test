c834642725724a23eac9605e747a29667206a617
KPIshnik <dimoglo.anton@gmail.com>
2022-08-27 15:35:46 +0000

    lesson 9

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,63 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send('Aloha Mather Fucker! \n');
+app.use(express_1.default.json());
+const db = {
+    users: [
+        { id: 1, username: "Io" },
+        { id: 2, username: "Fran" },
+        { id: 3, username: "Kris" },
+    ],
+};
+app.get("/", (req, res) => {
+    res.send("Aloha Mather Fucker! \n");
 });
-app.get('/huita', (req, res) => {
-    res.send('Huita is hapening \n');
+app.get("/users", (req, res) => {
+    let users = db.users;
+    if (req.query.username) {
+        users = users.filter((u) => u.username.indexOf(req.query.username) > -1);
+    }
+    res.json(users);
 });
-app.post('/huita', (req, res) => {
-    res.send('Huita is chenged');
+app.get("/users/:id", (req, res) => {
+    const user = db.users.find((u) => u.id === +req.params.id);
+    if (!user) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(user);
+});
+app.post("/users", (req, res) => {
+    if (!req.body.user.username) {
+        res.status(400).json({ err: "username required" });
+    }
+    db.users.push({ id: Date.now(), username: req.body.user.username });
+    const user = db.users.find((u) => u.username === req.body.user.username);
+    res.status(201).json(user);
+});
+app.delete("/users/:id", (req, res) => {
+    const user = db.users.find((u) => u.id === +req.params.id);
+    if (!user) {
+        res.sendStatus(404);
+        return;
+    }
+    db.users.splice(db.users.indexOf(user), 1);
+    res.json(db.users);
+});
+app.put("/users/:id", (req, res) => {
+    const user = db.users.find((u) => u.id === +req.params.id);
+    if (!user) {
+        res.sendStatus(404);
+        return;
+    }
+    if (!req.body.user.username) {
+        res.sendStatus(400);
+        return;
+    }
+    user.username = req.body.user.username;
+    res.json(user);
 });
 app.listen(3000, () => {
-    console.log('started server');
+    console.log("started server");
 });
 module.exports = app;
--- index.ts
+++ index.ts
@@ -1,21 +1,76 @@
-import express from 'express'
+import express from "express";
 
-const app=express()
+const app = express();
+app.use(express.json());
+const db = {
+	users: [
+		{ id: 1, username: "Io" },
+		{ id: 2, username: "Fran" },
+		{ id: 3, username: "Kris" },
+	],
+};
 
-app.get('/',(req,res)=>{
-    res.send('Aloha Mather Fucker! \n')
-})
+app.get("/", (req, res) => {
+	res.send("Aloha Mather Fucker! \n");
+});
 
-app.get('/huita',(req,res)=>{
-    res.send('Huita is hapening \n')
-})
+app.get("/users", (req, res) => {
+	let users = db.users;
 
-app.post('/huita',(req,res)=>{
-    res.send('Huita is chenged')
-})
+	if (req.query.username) {
+		users = users.filter(
+			(u) => u.username.indexOf(req.query.username as string) > -1
+		);
+	}
+	
+	res.json(users);
+});
+app.get("/users/:id", (req, res) => {
+	const user = db.users.find((u) => u.id === +req.params.id);
+	if (!user) {
+		res.sendStatus(404);
+		return;
+	}
 
-app.listen(3000, ()=> {
-    console.log('started server')
-})
+	res.json(user);
+});
 
-module.exports=app
+app.post("/users", (req, res) => {
+	if (!req.body.user.username) {
+		res.status(400).json({ err: "username required" });
+	}
+
+	db.users.push({ id: Date.now(), username: req.body.user.username });
+	const user = db.users.find((u) => u.username === req.body.user.username);
+
+	res.status(201).json(user);
+});
+app.delete("/users/:id", (req, res) => {
+	const user = db.users.find((u) => u.id === +req.params.id);
+	if (!user) {
+		res.sendStatus(404);
+		return;
+	}
+	db.users.splice(db.users.indexOf(user), 1);
+	res.json(db.users);
+});
+app.put("/users/:id", (req, res) => {
+	const user = db.users.find((u) => u.id === +req.params.id);
+	if (!user) {
+		res.sendStatus(404);
+		return;
+	}
+	if (!req.body.user.username) {
+		res.sendStatus(400);
+		return;
+	}
+
+	user.username = req.body.user.username;
+	res.json(user);
+});
+
+app.listen(3000, () => {
+	console.log("started server");
+});
+
+module.exports = app;
